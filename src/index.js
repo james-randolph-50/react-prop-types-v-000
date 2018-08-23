03308f0bc5e0df4e1ad2d8b23a36a2bfb95118e0
James Randolph <james.randolph2016@gmail.com>
2018-08-23 22:10:09 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-Order.propTypes = {
-  cone: PropTypes.bool,
-  size: PropTypes.string,
-  scoops: PropTypes.arrayOf(PropTypes.string).isRequired
-};
+ 
 
 Order.propTypes = {
   cone: PropTypes.bool,
