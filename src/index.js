b63e899a2b03bda87b35348d824b17f921babe49
James Randolph <james.randolph2016@gmail.com>
2018-08-23 22:09:47 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ Order.propTypes = {
   size: PropTypes.string,
   scoops: PropTypes.arrayOf(PropTypes.string).isRequired
 };
+
+Order.propTypes = {
+  cone: PropTypes.bool,
+  size: PropTypes.string,
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+  orderInfo: PropTypes.object.isRequired
+};
